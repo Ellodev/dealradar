a63191672e0f2190ea785de00b9c70f2465f66ce
Ellodev <69417638+Ellodev@users.noreply.github.com>
2025-01-17 09:16:52 +0100

    why doesnt it work, idk

--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,14 @@
-import NextAuth from "next-auth";
-import { authOptions } from "./config";
+import NextAuth  from "next-auth";
+import GithubProvider from "next-auth/providers/github"
 
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
+export const authOptions = {
+    providers: [GithubProvider({
+        clientId: process.env.GITHUB_ID ?? "",
+        clientSecret: process.env.GITHUB_SECRET ?? "",
+    }),
+]
+};
+
+export const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST};
