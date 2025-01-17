4c2a546c7087cfd85cb3c8a072aa323bd3dbcc11
Ellodev <69417638+Ellodev@users.noreply.github.com>
2025-01-17 09:21:00 +0100

    I dont understand this

--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,6 @@
 import NextAuth  from "next-auth";
-import GithubProvider from "next-auth/providers/github"
 
-export const authOptions = {
-    providers: [GithubProvider({
-        clientId: process.env.GITHUB_ID ?? "",
-        clientSecret: process.env.GITHUB_SECRET ?? "",
-    }),
-]
-};
+import { authOptions } from "./authOptions";
 
-export const handler = NextAuth(authOptions);
-
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST};
