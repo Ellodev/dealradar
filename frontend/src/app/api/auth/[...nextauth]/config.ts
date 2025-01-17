2576544c86e363b53ff89abddebddaf5590771b0
Ellodev <69417638+Ellodev@users.noreply.github.com>
2025-01-17 09:10:48 +0100

    fix route handler

---
+++ frontend/src/app/api/auth/[...nextauth]/config.ts
@@ -0,0 +1,10 @@
+import GithubProvider from "next-auth/providers/github"
+import type { AuthOptions } from "next-auth"
+
+export const authOptions: AuthOptions = {
+    providers: [GithubProvider({
+        clientId: process.env.GITHUB_ID ?? "",
+        clientSecret: process.env.GITHUB_SECRET ?? "",
+    }),
+]
+};
